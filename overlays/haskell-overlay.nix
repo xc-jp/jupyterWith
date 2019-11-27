32a87068185283a4d659ce7678562972b3a00b87
Viktor Kronvall <viktor.kronvall@gmail.com>
2019-11-27 18:32:52 +0900

    Add ihaskell-* packages from hackage

--- overlays/haskell-overlay.nix
+++ overlays/haskell-overlay.nix
@@ -2,6 +2,9 @@
 
 self: super:
 let
+  inherit (self) haskell-nix;
+  inherit (haskell-nix) mkPkgSet mkStackPkgSet stackage hackage callCabalToNix excludeBootPackages;
+
   importCabal = args: import "${callCabalToNix args}";
 
   getSubdir = repo:
@@ -20,15 +23,26 @@ let
       inherit name cabal-file;
     };
 
-  inherit (self) haskell-nix;
-  inherit (haskell-nix) mkPkgSet mkStackPkgSet stackage hackage callCabalToNix excludeBootPackages;
-
   pkg-set = mkStackPkgSet {
     stack-pkgs = {
       resolver = "lts-14.13";
       extras = hackage: {};
     };
-    pkg-def-extras = pkg-def-extras {
+    pkg-def-extras = [(hackage: {
+      ihaskell-aeson = hackage.ihaskell-aeson."0.3.0.1".revisions.default;
+      ihaskell-blaze = hackage.ihaskell-blaze."0.3.0.1".revisions.default;
+      ihaskell-charts = hackage.ihaskell-charts."0.3.0.1".revisions.default;
+      ihaskell-diagrams = hackage.ihaskell-diagrams."0.3.2.1".revisions.default;
+      ihaskell-gnuplot = hackage.ihaskell-gnuplot."0.1.0.1".revisions.default;
+      ihaskell-graphviz = hackage.ihaskell-graphviz."0.1.0.0".revisions.default;
+      ihaskell-hatex = hackage.ihaskell-hatex."0.2.1.1".revisions.default;
+      ihaskell-juicypixels = hackage.ihaskell-juicypixels."1.1.0.1".revisions.default;
+      ihaskell-magic = hackage.ihaskell-magic."0.3.0.1".revisions.default;
+      ihaskell-plot = hackage.ihaskell-plot."0.3.0.1".revisions.default;
+      ihaskell-rlangqq = hackage.ihaskell-rlangqq."0.3.0.0".revisions.default;
+      ihaskell-widgets = hackage.ihaskell-widgets."0.2.3.3".revisions.default;
+      ihaskell-hvega = hackage.ihaskell-hvega."0.2.0.2".revisions.default;
+    })] ++ pkg-def-extras {
       inherit haskell-nix;
     };
     modules = [{
